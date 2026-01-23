4b8f303bdc4164ef46b31ac9f99806b339eec0d3
Sergio <oliveira-sergio@outlook.com>
2026-01-23 18:28:58 +0000

    Create model_voucher.py
    act:
    -> model create by using boto3

---
+++ vouchers/model_voucher.py
@@ -0,0 +1,30 @@
+import uuid
+import boto3
+
+from vouchers.choices import VoucherStatus, CountryCode
+
+# Create your models here.
+
+class Voucher:
+
+    def __init__(self, voucher_description, voucher_price, voucher_quantity, voucher_status, voucher_location):
+        self.voucher_id = str(uuid.uuid4())[:8].upper()
+        self.voucher_description = voucher_description
+        self.voucher_price = str(voucher_price)
+        self.voucher_quantity = voucher_quantity
+        self.voucher_status = voucher_status
+        self.voucher_location = voucher_location
+
+
+    def to_dict(self):
+        """Converts the object to the format that DynamoDB supports"""
+        return {
+            'voucher_id': self.voucher_id,
+            'voucher_description': self.voucher_description,
+            'voucher_price': self.voucher_price,
+            'voucher_quantity': self.voucher_quantity,
+            'voucher_status': str(self.voucher_status),
+            'voucher_location': str(self.voucher_location)
+        }
+
+
