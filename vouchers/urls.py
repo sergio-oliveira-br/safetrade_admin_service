c20af3f214e3d20b80b13acc839010cdbddc4ab6
Sergio <oliveira-sergio@outlook.com>
2026-01-23 10:41:27 +0000

    Update urls.py
    act:
    -> urls added (3 page so far)

--- vouchers/urls.py
+++ vouchers/urls.py
@@ -1,8 +1,16 @@
 # vouchers/urls.py
 
 from django.urls import path
-from . import view_admin
+from . import view_vouchers, view_dashboard, view_index
 
 urlpatterns = [
-    path('', view_admin.index_page, name='admin'),
+    # Index
+    path('', view_index.index_page, name='index'),
+
+    # Admin
+    path('vouchers/', view_vouchers.vouchers_page, name='vouchers'),
+
+    # Dashboard
+    path('dashboard/', view_dashboard.dashboard_page, name='dashboard'),
+
 ]
