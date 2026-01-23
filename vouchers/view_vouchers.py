90e75fca4cfcc51430173ded71f257ab801a6ed5
Sergio <oliveira-sergio@outlook.com>
2026-01-23 18:30:19 +0000

    Update view_vouchers.py
    act:
    -> view created to render the page
    -> method to create a voucher by using the model was created too

--- vouchers/view_vouchers.py
+++ vouchers/view_vouchers.py
@@ -1,7 +1,64 @@
+# vouchers/view_vouchers.py
+
+from botocore.exceptions import ClientError
 from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render, redirect
+from django.views.decorators.http import require_http_methods
+
+from vouchers.forms import VoucherForm
+from vouchers.model_voucher import Voucher
+
 
 def vouchers_page(request):
-    return render(request, 'core/vouchers.html')
+    form = VoucherForm(request.POST or None)
+
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'core/vouchers.html', context)
+
+
+@require_http_methods(['GET', 'POST'])
+def create_voucher(request):
+
+    form = VoucherForm(request.POST or None)
+    success_message = None
+    error_message = None
+
+    if request.method == 'POST' and form.is_valid():
+
+        data = form.cleaned_data
+
+        try:
+            voucher = Voucher(
+                voucher_description=data['voucher_description'],
+                voucher_location=data['voucher_location'],
+                voucher_status=data['voucher_status'],
+                voucher_price=data['voucher_price'],
+                voucher_quantity=data['voucher_quantity']
+            )
+
+            voucher.save()
+            success_message = 'Voucher(s) was created successfully'
+
+            form = VoucherForm()
+
+        except ClientError as e:
+            error_message = str(e)
+
+        except Exception as e:
+            error_message = 'Sorry, something went wrong'
+
+    elif request.method == 'POST':
+        error_message = 'Check your input data and try again'
+
+    context = {
+        'form': form,
+        'success_message': success_message,
+        'error_message': error_message,
+    }
+
+    return render(request, 'core/vouchers.html', context)
