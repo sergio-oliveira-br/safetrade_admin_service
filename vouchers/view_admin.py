98000a47ebc31789c64b69a025b633c06f7e759b
Sergio <oliveira-sergio@outlook.com>
2026-01-23 10:38:33 +0000

    Delete view_admin.py
    name refactored

--- vouchers/view_admin.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render, redirect
-
-def index_page(request):
-    return render(request, 'core/admin.html')
