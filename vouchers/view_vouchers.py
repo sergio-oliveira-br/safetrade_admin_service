ca3baf60da4460b485c6c30771c02985ab171de5
Sergio <oliveira-sergio@outlook.com>
2026-01-29 12:41:41 +0000

    Update view_vouchers.py
    act:
    -> cleanup location
    
    -> status will replace the location as a Partition Key

--- vouchers/view_vouchers.py
+++ vouchers/view_vouchers.py
@@ -14,7 +14,7 @@ from vouchers.model_voucher import Voucher
 def vouchers_page(request):
     form = VoucherForm(request.POST or None)
 
-    voucher_list = Voucher.list_vouchers_by_location('BR')
+    voucher_list = Voucher.list_vouchers_by_status('Active')
 
     context = {
         'voucher_table': voucher_list,
