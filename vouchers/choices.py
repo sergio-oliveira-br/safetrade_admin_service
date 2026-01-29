7c96925cb4352f431fd9734d4c2549f20fa43db6
Sergio <oliveira-sergio@outlook.com>
2026-01-29 12:40:28 +0000

    Update choices.py
    act:
    -> cleanup location

--- vouchers/choices.py
+++ vouchers/choices.py
@@ -2,12 +2,6 @@
 
 from django.db import models
 
-class CountryCode(models.TextChoices):
-    BRAZIL = 'BR', 'Brazil'
-    IRELAND = 'IE', 'Ireland'
-    PORTUGAL = 'PT', 'Portugal'
-    UK = 'UK', 'United Kingdom'
-
 class VoucherStatus(models.TextChoices):
     ACTIVE = 'Active', 'Active'
     INACTIVE = 'Inactive', 'Inactive'
