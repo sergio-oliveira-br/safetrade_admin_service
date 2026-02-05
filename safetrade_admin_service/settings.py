85554b99baf62010e2600b90decd4994e3a26ca5
Sergio <oliveira-sergio@outlook.com>
2026-02-05 19:47:21 +0000

    Update settings.py

--- safetrade_admin_service/settings.py
+++ safetrade_admin_service/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-$$m*1=o78$)#+#hq5g9f&yy3inbafga#at+1$&k-)m3c!=n7!2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -74,12 +74,13 @@ WSGI_APPLICATION = "safetrade_admin_service.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+DATABASES = {}
 
 
 # Password validation
