86f9640f1a66dfd76033b7a4ba73eeb27b338589
Sergio <oliveira-sergio@outlook.com>
2026-02-06 23:08:27 +0000

    Update view_edit_voucher.py
    act:
    -> context added

--- vouchers/view_edit_voucher.py
+++ vouchers/view_edit_voucher.py
@@ -3,5 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render, redirect
 
+from vouchers.forms import VoucherForm
+
+
+def _get_vouchers_context(form=None, success=None, error=None):
+    """Helper to avoid code repetition in the context"""
+    return {
+        'form': form or VoucherForm(),
+        'success_message': success,
+        'error_message': error,
+    }
+
 def edit_voucher_page(request):
-    return render(request, 'core/edit_voucher.html')
+    return render(request, 'core/edit_voucher.html', _get_vouchers_context(form=VoucherForm(), success=False, error=None))
