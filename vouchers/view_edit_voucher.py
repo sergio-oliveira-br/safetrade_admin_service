c1268536746f63c26c135106e657d0b31c001229
Sergio <oliveira-sergio@outlook.com>
2026-02-07 00:21:03 +0000

    Update view_edit_voucher.py
    act:
    -> method eliminated to simplified

--- vouchers/view_edit_voucher.py
+++ vouchers/view_edit_voucher.py
@@ -6,13 +6,6 @@ from django.shortcuts import render, redirect
 from vouchers.forms import VoucherForm
 
 
-def _get_vouchers_context(form=None, success=None, error=None):
-    """Helper to avoid code repetition in the context"""
-    return {
-        'form': form or VoucherForm(),
-        'success_message': success,
-        'error_message': error,
-    }
 
 def edit_voucher_page(request):
     return render(request, 'core/edit_voucher.html', _get_vouchers_context(form=VoucherForm(), success=False, error=None))
