4b6842b0f472550e4fa0575f8f252310361e4e69
Sergio <oliveira-sergio@outlook.com>
2026-02-08 11:16:33 +0000

    Create form_for_edition.py

---
+++ vouchers/forms/form_for_edition.py
@@ -0,0 +1,13 @@
+# vouchers/forms/form_for_edition.py
+from django import forms
+
+from vouchers.forms.form_base import VoucherBaseForm
+
+
+class VoucherEditionForm(VoucherBaseForm):
+    voucher_tx_hash = forms.CharField(
+        label='Voucher Tx Hash',
+        min_length=1,
+        max_length=50,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
