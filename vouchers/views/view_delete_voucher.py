e6f98f4233298560cb8594b5a6d7a2e6c84d1fbb
Sergio <oliveira-sergio@outlook.com>
2026-02-10 21:34:52 +0000

    Update view_delete_voucher.py
    act:
    new method created to invoke the service method to delete an item

--- vouchers/views/view_delete_voucher.py
+++ vouchers/views/view_delete_voucher.py
@@ -1,5 +1,5 @@
 from django.contrib import messages
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from vouchers.model_voucher import Voucher
 
@@ -13,3 +13,20 @@ def delete_voucher_page(request, voucher_id):
         'voucher_id': voucher_id,
     }
     return render(request, 'core/delete_voucher.html', context)
+
+def delete_voucher(request, voucher_id):
+
+    voucher = Voucher.find_voucher_by_id(voucher_id)
+
+    if not voucher:
+        messages.error(request, f"Voucher {voucher_id} not found")
+
+    # Invoke the service method to delete the item from DB
+    delete_service = Voucher.delete_voucher(voucher_id)
+
+    if not delete_service['success']:
+        messages.error(request, delete_service['message'])
+        return redirect('vouchers')
+
+    messages.success(request, f"Voucher ID: {voucher_id} deleted successfully")
+    return redirect('vouchers')
