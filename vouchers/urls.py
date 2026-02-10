6d4f6699d62f3b2d05bf3ad8dd3cf08f89db8446
Sergio <oliveira-sergio@outlook.com>
2026-02-10 21:34:15 +0000

    Update urls.py
    act:
    -> route added

--- vouchers/urls.py
+++ vouchers/urls.py
@@ -15,5 +15,6 @@ urlpatterns = [
     path('edit_voucher/<str:voucher_id>/', view_edit_voucher.edit_voucher_page, name='edit_page'),
 
     path('delete_voucher/<str:voucher_id>', view_delete_voucher.delete_voucher_page, name='delete_page'),
+    path('delete_voucher/<str:voucher_id>', view_delete_voucher.delete_voucher, name='delete_voucher'),
 
 ]
