4afad88c587a87b92249955eb8f402353c9c74f3
Sergio <oliveira-sergio@outlook.com>
2026-02-10 21:06:37 +0000

    Update view_delete_voucher.py
    act:
    -> object added to render in the page

--- vouchers/view_delete_voucher.py
+++ vouchers/view_delete_voucher.py
@@ -1,8 +1,15 @@
 from django.contrib import messages
 from django.shortcuts import render
 
+from vouchers.model_voucher import Voucher
+
+
 def delete_voucher_page(request, voucher_id):
+
+    voucher = Voucher.find_voucher_by_id(voucher_id)
+
     context = {
+        'voucher': voucher,
         'voucher_id': voucher_id,
     }
     return render(request,'core/delete_voucher.html', context )
