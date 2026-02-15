e9f696c35e8df3832b74761fee584a36da918d13
Sergio <oliveira-sergio@outlook.com>
2026-02-15 14:43:03 +0000

    Update view_dashboard.py

--- vouchers/views/view_dashboard.py
+++ vouchers/views/view_dashboard.py
@@ -3,5 +3,21 @@ from django.shortcuts import render
 # Create your views here.
 from django.shortcuts import render, redirect
 
+from vouchers.model_voucher import Voucher
+
+
 def dashboard_page(request):
-    return render(request, 'core/dashboard.html')
+
+    active_count = Voucher.count_vouches_by_status('Active')
+    pending_count = Voucher.count_vouches_by_status('Pending')
+    sold_count = Voucher.count_vouches_by_status('Sold')
+    gifted_count = Voucher.count_vouches_by_status('Gifted')
+
+    context = {
+        'active_count': active_count,
+        'pending_count': pending_count,
+        'sold_count': sold_count,
+        'gifted_count': gifted_count,
+    }
+
+    return render(request, 'core/dashboard.html', context)
