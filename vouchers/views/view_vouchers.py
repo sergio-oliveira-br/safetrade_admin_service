476d9f4172ef12028a655de70c77e2c200ca4807
Sergio <oliveira-sergio@outlook.com>
2026-02-16 16:13:30 +0000

    Update view_vouchers.py
    cleanup

--- vouchers/views/view_vouchers.py
+++ vouchers/views/view_vouchers.py
@@ -1,6 +1,6 @@
 # vouchers/view_vouchers.py
 from django.core.paginator import Paginator
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from django.views.decorators.http import require_http_methods
 from vouchers.forms.form_for_creation import VoucherCreationForm
 from vouchers.model_voucher import Voucher
