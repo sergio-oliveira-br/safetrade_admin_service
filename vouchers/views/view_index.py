001d289c383dfcc93aa94ec0622dc031ab9e9590
Sergio <oliveira-sergio@outlook.com>
2026-02-16 16:46:34 +0000

    Update view_index.py

--- vouchers/views/view_index.py
+++ vouchers/views/view_index.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.shortcuts import render
-from django.views.decorators.http import require_safe
+from django.views.decorators.http import require_GET
 
 
-@require_safe
+@require_GET
 def index_page(request):
     return render(request, 'core/pages/index.html')
