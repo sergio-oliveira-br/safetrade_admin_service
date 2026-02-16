aac3404be1680ca0913d8449c25b84d65d69ee9a
Sergio <oliveira-sergio@outlook.com>
2026-02-16 16:42:15 +0000

    Update view_index.py
    issue
    # Make sure allowing safe and unsafe HTTP methods is safe here.
    
    act:
    -> For all the routes/controllers of an application, the authorized HTTP methods should be explicitly defined and safe HTTP methods should only be used to perform read-only operations. (SonarQube)

--- vouchers/views/view_index.py
+++ vouchers/views/view_index.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.shortcuts import render, redirect
+from django.shortcuts import render
+from django.views.decorators.http import require_safe
 
+
+@require_safe
 def index_page(request):
     return render(request, 'core/pages/index.html')
